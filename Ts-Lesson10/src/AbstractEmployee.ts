c61cce95477b3225a84da4d5a6cb28332c0153d4
MikenasIgnas <mikenasignas@gmail.com>
2022-08-28 13:11:07 +0300

    Lesson 10 Tasks

--- Ts-Lesson10/src/AbstractEmployee.ts
+++ Ts-Lesson10/src/AbstractEmployee.ts
@@ -1,7 +1,6 @@
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 import Person from './AbstractPerson.js';
-
 abstract class Employee extends Person {
     public abstract calcPay():number
 }
